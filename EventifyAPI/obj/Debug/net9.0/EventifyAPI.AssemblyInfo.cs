37e0f4952196a4c9a95e50760eaa6238b7f09936
Molele Glenh <112684142+s225282372@users.noreply.github.com>
2025-09-19 04:37:35 +0200

    updated Events Dto
    Added comments and missing attributes

--- EventifyAPI/obj/Debug/net9.0/EventifyAPI.AssemblyInfo.cs
+++ EventifyAPI/obj/Debug/net9.0/EventifyAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f69beab39726523c98c6ad4e37ddb79f3a8bf8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ed217ec61760e3cefb2a045e753beea674108d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
