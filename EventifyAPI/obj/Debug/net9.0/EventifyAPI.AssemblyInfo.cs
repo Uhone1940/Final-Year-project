e6b0b6f2a4ae447563650d1310b71531c2daf350
Molele Glenh <112684142+s225282372@users.noreply.github.com>
2025-09-21 01:22:29 +0200

    new migration added
    created a new initial migration

--- EventifyAPI/obj/Debug/net9.0/EventifyAPI.AssemblyInfo.cs
+++ EventifyAPI/obj/Debug/net9.0/EventifyAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f1bc3094c6d6d775e037ac240f313d9f064029")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfa1fb15e7497713f44d913d9675913b628df3a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
