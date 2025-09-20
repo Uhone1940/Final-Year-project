95a7284f3814a060a16e7b514077d756077f138d
Molele Glenh <112684142+s225282372@users.noreply.github.com>
2025-09-21 00:45:11 +0200

    deleted the migrations

--- EventifyAPI/obj/Debug/net9.0/EventifyAPI.AssemblyInfo.cs
+++ EventifyAPI/obj/Debug/net9.0/EventifyAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aae9dd09d69a178b266b65a0729a773f5ca9caa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b14209f2b022087667ec5a0adb5eeeb31f4012b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
