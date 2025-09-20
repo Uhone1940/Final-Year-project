17e0ba6346f2b0bd105715e765f5552b8019b386
Molele Glenh <112684142+s225282372@users.noreply.github.com>
2025-09-20 22:36:49 +0200

    added migrations to the database
    suspension migration

--- EventifyAPI/obj/Debug/net9.0/EventifyAPI.AssemblyInfo.cs
+++ EventifyAPI/obj/Debug/net9.0/EventifyAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca0544e525c48abe04d18cef16244d89ca45020")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb6d3da0da43225f7e3e3d57b0b2d76b2c98bc34")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
