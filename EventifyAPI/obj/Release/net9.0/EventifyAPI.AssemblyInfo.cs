43525c7546ab9d80616a4c9ccd95657cb69ff9ac
Molele Glenh <112684142+s225282372@users.noreply.github.com>
2025-10-02 00:19:06 +0200

    Testing the APIs

--- EventifyAPI/obj/Release/net9.0/EventifyAPI.AssemblyInfo.cs
+++ EventifyAPI/obj/Release/net9.0/EventifyAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca0544e525c48abe04d18cef16244d89ca45020")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6605af5956922d6972632024f079d3db6fb4b596")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
