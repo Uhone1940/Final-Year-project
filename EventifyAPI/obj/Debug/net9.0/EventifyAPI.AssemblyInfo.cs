9f128ce4949ef4ac5e4201b00d2ab721f5b9abc1
Molele Glenh <112684142+s225282372@users.noreply.github.com>
2025-10-29 02:00:37 +0200

    updated the database

--- EventifyAPI/obj/Debug/net9.0/EventifyAPI.AssemblyInfo.cs
+++ EventifyAPI/obj/Debug/net9.0/EventifyAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cae4b32fdbb06dd67e92a6753b9e113283f935f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c1c843dfa1e48f36b5b6369b5f5c751d87ddf46")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventifyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
